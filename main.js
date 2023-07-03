723dd5c1f7397637812bcf0fbc94f475df77bcd5
Naomi Rider <Naomi.Rider@softwire.com>
2023-07-03 15:21:08 +0100

    Add part 2

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ function divByNumber(div, num){
     return num % div == 0;
 }
 
+// Reverses the order in which any fizzes, buzzes, bangs etc. are printed
+function reverseOrder(str){
+    const splitString = str.split(/(?=[A-Z])/);
+    return splitString.reverse().join('').toString();
+}
+
 function fizzbuzz() {
 
     for (let i = 1; i < 101; i++){
@@ -16,6 +22,28 @@ function fizzbuzz() {
 
         if (divByNumber(5, i)){
             stringToPrint += 'Buzz';
+        }
+
+        if (divByNumber(7, i)){
+            stringToPrint += 'Bang';
+        }
+
+        if (divByNumber(11, i)){
+            stringToPrint = 'Bong';
+        }
+
+        if (divByNumber(13, i)){
+            const indexB = stringToPrint.indexOf('B');
+            if (indexB != -1){
+                stringToPrint = stringToPrint.slice(0, indexB) + 'Fezz' + stringToPrint.slice(indexB);
+            }
+            else{
+                stringToPrint += 'Fezz';
+            }
+        }
+
+        if (divByNumber(17, i)){
+            stringToPrint = reverseOrder(stringToPrint)
         }
 
         if (stringToPrint == ''){
