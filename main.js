c93c9e8b160d4adf8481bc3da2a2ef577a91b080
Naomi Rider <Naomi.Rider@softwire.com>
2023-07-03 15:33:44 +0100

    Prompt the user for a maximum number

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Main function
+const readlineSync = require('readline-sync');
 
 // Divides a provided number by a divisor
 function divByNumber(div, num){
@@ -11,9 +11,12 @@ function reverseOrder(str){
     return splitString.reverse().join('').toString();
 }
 
+// Main function
 function fizzbuzz() {
+    var upToX = readlineSync.question('Please enter the number you would like to FizzBuzz etc. up to: ');
+    upToX = +upToX;
 
-    for (let i = 1; i < 101; i++){
+    for (let i = 1; i < upToX + 1; i++){
         var stringToPrint = '';
 
         if (divByNumber(3, i)){
