b8428fffa789e590873e350edfe60ce58faa19d7
Naomi Rider <Naomi.Rider@softwire.com>
2023-07-03 14:58:20 +0100

    Add part 1

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
 // Main function
-function fizzbuzz() {
-    console.log("Hello, world!")
 
-    // Code goes here...
+// Divides a provided number by a divisor
+function divByNumber(div, num){
+    return num % div == 0;
+}
+
+function fizzbuzz() {
+
+    for (let i = 1; i < 101; i++){
+        var stringToPrint = '';
+
+        if (divByNumber(3, i)){
+            stringToPrint += 'Fizz';
+        }
+
+        if (divByNumber(5, i)){
+            stringToPrint += 'Buzz';
+        }
+
+        if (stringToPrint == ''){
+            stringToPrint = i;
+        }
+        
+        console.log(stringToPrint);
+    }
 }
 
 // Call main function
